8cd77cf81c6dbf2c488a6f1d1e1412c8a78c29d9
Christian Baron <c.baron@neusta.de>
2021-02-18 15:00:29 +0100

    empty vod fragment

--- app/src/main/java/com/gfk/s2s/sensic_demo_app_android/DashboardFragment.kt
+++ app/src/main/java/com/gfk/s2s/sensic_demo_app_android/DashboardFragment.kt
@@ -5,12 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 
 class DashboardFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+        val view =  inflater.inflate(R.layout.fragment_dashboard, container, false)
+        view.findViewById<Button>(R.id.vodButton).setOnClickListener {
+            findNavController().navigate(R.id.actionDashBoardFragment_toVODFragment)
+        }
+        return view
+
     }
 
 }
