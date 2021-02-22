4add8bca9b08ff6a4fef4699bb6ec20fa070bc1e
Christian Baron <c.baron@neusta.de>
2021-02-22 11:08:36 +0100

    added dependency to s2s

--- app/src/main/java/com/gfk/s2s/sensic_demo_app_android/VODFragment.kt
+++ app/src/main/java/com/gfk/s2s/sensic_demo_app_android/VODFragment.kt
@@ -5,10 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.gfk.s2s.s2sagent.S2SAgent
 
 class VODFragment : Fragment() {
 
+    val videoURL = "https://demo-config-preproduction.sensic.net/video/video3.mp4"
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.vod_fragment,  container, false)
+        return inflater.inflate(R.layout.vod_fragment, container, false)
     }
 }
