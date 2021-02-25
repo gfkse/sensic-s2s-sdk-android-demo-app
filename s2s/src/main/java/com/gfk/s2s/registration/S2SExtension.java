99d1b3c6e987813208f6e89fc00f4809629e2b8a
Christian Baron <c.baron@neusta.de>
2021-02-25 13:56:01 +0100

    removed code

--- s2s/src/main/java/com/gfk/s2s/registration/S2SExtension.java
+++ s2s/src/main/java/com/gfk/s2s/registration/S2SExtension.java
@@ -1,8 +1,5 @@
 package com.gfk.s2s.registration;
 
-import android.os.Handler;
-import android.util.Log;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
@@ -39,9 +36,6 @@ public class S2SExtension {
     }
 
     public void bindPlayer(@NonNull S2SAgent agent, @NonNull ExoPlayer player) {
-        if (agent == null || player == null) {
-            return;
-        }
         this.exoPlayer = player;
         this.s2sAgent = agent;
         this.s2sAgent.setStreamPositionCallback(() -> player.getCurrentPosition() > 1000 ? (int) player.getCurrentPosition() : 0);
@@ -49,23 +43,16 @@ public class S2SExtension {
 
             @Override
             public void onPositionDiscontinuity(int reason) {
-                if (reason == ExoPlayer.DISCONTINUITY_REASON_SEEK) {
+                if (reason == ExoPlayer.DISCONTINUITY_REASON_SEEK || reason == ExoPlayer.DISCONTINUITY_REASON_SEEK_ADJUSTMENT) {
                     hugeJump = true;
-                    Log.d("GFK_LOG", "DISCONTINUITY_REASON_SEEK " + exoPlayer.isPlaying());
-
-                } else if (reason == ExoPlayer.DISCONTINUITY_REASON_SEEK_ADJUSTMENT) {
-                    Log.d("GFK_LOG", "DISCONTINUITY_REASON_SEEK_ADJUSTMENT");
                 }
             }
 
             @Override
             public void onIsPlayingChanged(boolean isPlaying) {
-                Log.d("GFK_LOG", "onIsPlayingChanged " + isPlaying);
-
                 if (!isPlaying && lastEvent == SensicEvent.play) {
                     agent.stop(null);
                     lastEvent = SensicEvent.stop;
-                    Log.d("GFK_LOG", "onIsPlayingChanged AGENT STOP");
                 }
             }
 
@@ -74,14 +61,11 @@ public class S2SExtension {
                 switch (playbackState) {
                     case ExoPlayer.STATE_IDLE:
                         // not able to play
-                        Log.d("GFK_LOG", "STATE_IDLE");
                         break;
                     case ExoPlayer.STATE_BUFFERING:
                         // not enough data. also every seek
-                        Log.d("GFK_LOG", "STATE_BUFFERING");
                         break;
                     case ExoPlayer.STATE_ENDED:
-                        Log.d("GFK_LOG", "STATE_ENDED");
                         // called once at the end of the video or after skipping (STATE_BUFFERING -> STATE_ENDED -> STATE_READY)
                         // we use onIsPlayingChanged for this case
                         break;
@@ -90,26 +74,19 @@ public class S2SExtension {
                         if (hugeJump) {
                             if (lastEvent == SensicEvent.stop) {
                                 agent.playStreamOnDemand(contentId, videoUrl, customParams);
-                                Log.d("GFK_LOG", "STATE_READY IS hugeJump PLAY EVENT");
                             }
-                            Log.d("GFK_LOG", "STATE_READY IS hugeJump STOP EVENT " + exoPlayer.getCurrentPosition());
                             agent.stop(exoPlayer.getCurrentPosition());
                             lastEvent = SensicEvent.stop;
                             hugeJump = false;
                         }
 
                         if (exoPlayer.isPlaying()) {
-                            Log.d("GFK_LOG", "STATE_READY IS_PLAYING");
-
                             if (lastEvent != SensicEvent.play) {
-                                Log.d("GFK_LOG", "STATE_READY IS PLAYING PLAY EVENT");
                                 s2sAgent.playStreamOnDemand(contentId, videoUrl, customParams);
                                 lastEvent = SensicEvent.play;
                             }
                         } else {
-                            Log.d("GFK_LOG", "STATE_READY IS PAUSED");
                             if (lastEvent != SensicEvent.stop) {
-                                Log.d("GFK_LOG", "STATE_READY IS PAUSED STOP EVENT");
                                 s2sAgent.stop(null);
                                 lastEvent = SensicEvent.stop;
                             }
