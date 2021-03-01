893bf56b787a4759f3e72866780fdaf6adb282b5
Christian Baron <c.baron@neusta.de>
2021-03-01 15:28:41 +0100

    removed s2s Project. exoplayer.release()

--- app/src/main/java/com/gfk/s2s/sensic_demo_app_android/BaseVideoFragment.kt
+++ app/src/main/java/com/gfk/s2s/sensic_demo_app_android/BaseVideoFragment.kt
@@ -11,8 +11,8 @@ import android.widget.FrameLayout
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.gfk.s2s.registration.S2SExtension
 import com.gfk.s2s.s2sagent.S2SAgent
+import com.gfk.s2s.S2SExtension.S2SExtension
 import com.google.android.exoplayer2.DefaultLoadControl
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.SimpleExoPlayer
@@ -41,6 +41,77 @@ open class BaseVideoFragment : Fragment() {
         setupS2sAgent()
     }
 
+    private fun setupS2sAgent() {
+        agent = S2SAgent(configUrl, mediaId, context)
+        val s2sExtension = S2SExtension(mediaId, videoURL, null)
+        s2sExtension.bindPlayer(agent!!, exoPlayer!!)
+    }
+
+    private fun prepareVideoPlayer() {
+        val trackSelector = DefaultTrackSelector(requireContext())
+        val loadControl = DefaultLoadControl()
+
+        exoPlayer = SimpleExoPlayer.Builder(requireContext()).setTrackSelector(trackSelector)
+                .setLoadControl(loadControl).build()
+        val userAgent = Util.getUserAgent(
+                requireContext(), context?.getString(R.string.app_name)
+                ?: ""
+        )
+        val mediaSource =
+                ProgressiveMediaSource.Factory(DefaultDataSourceFactory(context, userAgent))
+                        .createMediaSource(Uri.parse(videoURL))
+
+        view?.findViewById<PlayerView>(R.id.videoView)?.player = exoPlayer
+        exoPlayer?.prepare(mediaSource)
+        exoPlayer?.seekTo(playerPosition)
+        exoPlayer?.playWhenReady = true
+    }
+
+    override fun onResume() {
+        super.onResume()
+        (activity as? MainActivity)?.usePictureInPictureByHomeButtonPress = true
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N &&
+                activity?.packageManager?.hasSystemFeature(PackageManager.FEATURE_PICTURE_IN_PICTURE) == true
+        ) {
+            if (activity?.isInPictureInPictureMode == false) {
+                exoPlayer?.playWhenReady = true
+            }
+        } else {
+            exoPlayer?.playWhenReady = true
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N &&
+                activity?.packageManager?.hasSystemFeature(PackageManager.FEATURE_PICTURE_IN_PICTURE) == true
+        ) {
+            if (activity?.isInPictureInPictureMode == false) {
+                playerPosition = exoPlayer?.currentPosition ?: 0
+                exoPlayer?.playWhenReady = false
+            }
+        } else {
+            playerPosition = exoPlayer?.currentPosition ?: 0
+            exoPlayer?.playWhenReady = false
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        exoPlayer?.release()
+    }
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putLong("playerPosition", playerPosition);
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+        playerPosition = savedInstanceState?.getLong("playerPosition") ?: 0
+    }
+
     override fun onConfigurationChanged(newConfig: Configuration) {
         super.onConfigurationChanged(newConfig)
         val mediaFrame = view?.findViewById<FrameLayout>(R.id.main_media_frame)
@@ -55,45 +126,6 @@ open class BaseVideoFragment : Fragment() {
         }
     }
 
-    private fun setupS2sAgent() {
-        agent = S2SAgent(configUrl, mediaId, context)
-        val s2sExtension = S2SExtension(mediaId, videoURL, null)
-        s2sExtension.bindPlayer(agent!!, exoPlayer!!)
-    }
-
-    private fun prepareVideoPlayer() {
-        val trackSelector = DefaultTrackSelector(requireContext())
-        val loadControl = DefaultLoadControl()
-
-        exoPlayer = SimpleExoPlayer.Builder(requireContext()).setTrackSelector(trackSelector)
-            .setLoadControl(loadControl).build()
-        val userAgent = Util.getUserAgent(
-            requireContext(), context?.getString(R.string.app_name)
-                ?: ""
-        )
-        val mediaSource =
-            ProgressiveMediaSource.Factory(DefaultDataSourceFactory(context, userAgent))
-                .createMediaSource(Uri.parse(videoURL))
-
-        view?.findViewById<PlayerView>(R.id.videoView)?.player = exoPlayer
-        exoPlayer?.prepare(mediaSource)
-        exoPlayer?.seekTo(playerPosition)
-        exoPlayer?.playWhenReady = true
-    }
-
-    override fun onResume() {
-        super.onResume()
-        (activity as? MainActivity)?.usePictureInPictureByHomeButtonPress = true
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N &&
-            activity?.packageManager?.hasSystemFeature(PackageManager.FEATURE_PICTURE_IN_PICTURE) == true
-        ) {
-            if (activity?.isInPictureInPictureMode == false) {
-                exoPlayer?.playWhenReady = true
-            }
-        } else {
-            exoPlayer?.playWhenReady = true
-        }
-    }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
@@ -101,30 +133,5 @@ open class BaseVideoFragment : Fragment() {
         }
         return true
 
-    }
-
-    override fun onPause() {
-        super.onPause()
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N &&
-            activity?.packageManager?.hasSystemFeature(PackageManager.FEATURE_PICTURE_IN_PICTURE) == true
-        ) {
-            if (activity?.isInPictureInPictureMode == false) {
-                playerPosition = exoPlayer?.currentPosition ?: 0
-                exoPlayer?.playWhenReady = false
-            }
-        } else {
-            playerPosition = exoPlayer?.currentPosition ?: 0
-            exoPlayer?.playWhenReady = false
-        }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putLong("playerPosition", playerPosition);
-    }
-
-    override fun onViewStateRestored(savedInstanceState: Bundle?) {
-        super.onViewStateRestored(savedInstanceState)
-        playerPosition = savedInstanceState?.getLong("playerPosition") ?: 0
     }
 }
